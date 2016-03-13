1fc41df777d871ca528474bf889591260b16e0aa
Flavio Andrade <flavio.andrade@locaweb.com.br>
2016-03-13 15:50:57 -0300

    Add umbrella project

---
+++ apps/kv/lib/kv/registry.ex
@@ -0,0 +1,51 @@
+defmodule KV.Registry do
+  use GenServer
+
+  def start_link(name) do
+    GenServer.start_link(__MODULE__, name, name: name)
+  end
+
+  def lookup(server, name) when is_atom(server) do
+    case :ets.lookup(server, name) do
+      [{^name, bucket}] -> {:ok, bucket}
+      [] -> :error
+    end
+  end
+
+  def create(server, name) do
+    GenServer.call(server, {:create, name})
+  end
+
+  def stop(server) do
+    GenServer.stop(server)
+  end
+
+  def init(table) do
+    names = :ets.new(table, [:named_table, read_concurrency: true])
+    refs = %{}
+    {:ok, {names, refs}}
+  end
+
+  def handle_call({:create, name}, _from, {names, refs}) do
+    case lookup(names, name) do
+      {:ok, pid} ->
+        {:reply, pid, {names, refs}}
+      :error ->
+        {:ok, pid} = KV.Bucket.Supervisor.start_bucket()
+        ref = Process.monitor(pid)
+        refs = Map.put(refs, ref, name)
+        :ets.insert(names, {name, pid})
+        {:reply, pid, {names, refs}}
+    end
+  end
+
+  def handle_info({:DOWN, ref, :process, _pid, reason}, {names, refs}) do
+    {name, refs} = Map.pop(refs, ref)
+    :ets.delete(names, name)
+    {:noreply, {names, refs}}
+  end
+
+  def handle_info(_msg, state) do
+    {:noreply, state}
+  end
+end
